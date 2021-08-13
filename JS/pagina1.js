ef789fbd159698a84a6047475a259d29210a6ff0
adriel <adrielsaul@gmail.com>
2021-08-13 00:39:38 -0300

    mudança no nome do repositorio

--- JS/pagina1.js
+++ JS/pagina1.js
@@ -3,13 +3,13 @@ function escolha() {
         var caminho = prompt('Qual Digimon irá te acompanhar nessa aventura: 1 - Patamon, 2 - Palmon ou 3 - V-Mon ? Digite o número');
 
         if (caminho == 1) {
-            return location = ('../Projeto Modulo 1/HTML/pagina2.html');
+            return location = ('./HTML/pagina2.html');
         }
         else if (caminho == 2) {
-            return location = ('../Projeto Modulo 1/HTML/palmon1.html');
+            return location = ('./HTML/palmon1.html');
         }
         else if (caminho == 3) {
-            return location = ('..//Projeto Modulo 1/HTML/vmon1.html');
+            return location = ('./HTML/vmon1.html');
         }
         else {
             alert('Digite uma opção válida');
