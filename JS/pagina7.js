9b2b6ebf3eae7143ef39d317052f346d622da528
adriel <adrielsaul@gmail.com>
2021-08-12 23:22:24 -0300

    mudança codigo js e novas paginas

---
+++ JS/pagina7.js
@@ -0,0 +1,15 @@
+function escolha() {
+    while (true) {
+        var caminho = prompt('O que fazer? 1 - Atacar BlackWargreymon ou 2 - Usar o Digiovo? Digite o número da opção');
+        
+        if (caminho == 1) {
+            return location = ('');
+        
+        } else if (caminho == 2) {
+            return location = ('');
+       
+        } else {
+            alert('Escolha uma opção válida');
+        }
+    }
+}
