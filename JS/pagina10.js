590871898f288bf8e34a4bed5c52aa38bc092db3
adriel <adrielsaul@gmail.com>
2021-08-18 11:06:10 -0300

    responsivos paginas 15 a 19, alteração js pg 10 para incluir alerta

--- JS/pagina10.js
+++ JS/pagina10.js
@@ -3,13 +3,15 @@ function escolha() {
         var caminho = prompt('qual a qualidade que você se identifica. 1 - Coragem,  2 - Esperança ou 3 - Conhecimento.');
 
         if (caminho == 1) {
-            return location = ('./pagina12.html');
+            alert("Culumon reiniciou a linha do tempo. Você deve escolher um Digimon novamente");
+            return location = ('../index.html');
         }
         else if (caminho == 2) {
             return location = ('./pagina11.html');
         }
         else if (caminho == 3) {
-            return location = ('./pagina12.html');
+            alert("Culumon reiniciou a linha do tempo. Você deve escolher um Digimon novamente");
+            return location = ('../index.html');
         }
         else {
             alert('Digite uma opção válida');
